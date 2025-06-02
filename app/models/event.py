d02a2a750244640198976e8079d7f9a20e3c7852
PGer871 <pietrogermano982@gmail.com>
2025-06-02 11:28:45 +0200

    Update event.py

--- app/models/event.py
+++ app/models/event.py
@@ -1,7 +1,11 @@
 from sqlmodel import SQLModel, Field
+from typing import Optional
+from datetime import datetime
 
-class User(SQLModel, table=True):
-    username: str = Field(primary_key=True)
-    name: str
-    email: str
+class Event(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title: str
+    description: str
+    date: datetime
+    location: str
 
